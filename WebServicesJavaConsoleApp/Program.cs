ddfa51a9fabce451b0f03db131545850a9b28b59
Szymon Bakunowicz <zorzin0os@gmail.com>
2019-03-26 00:29:51 +0100

    Init

---
+++ WebServicesJavaConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WeirdService;
+
+namespace WebServicesJavaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wh = new WorldHelloClient();
+
+            var products = wh.ProductsAsync().Result.@return;
+
+            foreach (var product in products)
+            {
+                Console.WriteLine("Otrzymano produkt {0}, o opisie {1} i cenie {2}", product.nazwa, product.opis, product.cena);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
